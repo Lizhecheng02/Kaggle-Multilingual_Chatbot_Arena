4092aa636e9aef3750484c5de1e17cd7e0a4604b
unknown <2569618533@qq.com>
2024-12-24 13:48:50 -0800

    try many-shot icl evaluator

---
+++ msicl_evaluator/main.py
@@ -0,0 +1,29 @@
+from utils import get_response_openai_prompt, sample_validation_dataset, construct_prompt
+from tqdm import tqdm
+
+
+def main(model, msicl_nums, file_path, sample_size):
+    sample_validation_df, remain_df = sample_validation_dataset(file_path, sample_size)
+    for msicl_num in msicl_nums:
+        final_sample_size = sample_size
+        acc_count = 0
+        pre_prompt = construct_prompt(df=remain_df, msicl_num=msicl_num)
+        print(f"Processing MSICL With {msicl_num} Examples")
+        for idx, row in tqdm(sample_validation_df.iterrows(), total=len(sample_validation_df)):
+            prompt = pre_prompt + f"<Question>\n{row['prompt']}\n</Question>\n"
+            prompt += f"<Response A>\n{row['response_a']}\n</Response A>\n"
+            prompt += f"<Response B>\n{row['response_b']}\n</Response B>\n"
+            prompt += "[YOUR JUDEMENT]:"
+            response = get_response_openai_prompt(model, prompt)
+            if response == "model_a" and row["winner"] == "model_a":
+                acc_count += 1
+            elif response == "model_b" and row["winner"] == "model_b":
+                acc_count += 1
+            elif response != "model_a" and response != "model_b":
+                print(f"Error: {response} Row: {idx + 1}")
+                final_sample_size -= 1
+        print(f"MSICL With {msicl_num} Examples Accuracy: {acc_count / final_sample_size * 100: .2f}%")
+
+
+if __name__ == "__main__":
+    main(model="gpt-4o-mini", msicl_nums=[2, 4], file_path="../original_data/train.parquet", sample_size=300)
---
+++ msicl_evaluator/utils.py
@@ -0,0 +1,72 @@
+import os
+import pandas as pd
+import yaml
+import warnings
+from openai import OpenAI
+warnings.filterwarnings("ignore")
+
+config_path = os.path.join(os.getcwd(), "..", "config.yaml")
+
+with open(config_path, "r") as file:
+    config = yaml.safe_load(file)
+
+openai_api_key = config["openai"]["api_key"]
+openai_organization = config["openai"]["organization"]
+client_openai = OpenAI(api_key=openai_api_key, organization=openai_organization)
+
+
+def get_response_openai_prompt(model, prompt, temperature=0.0, top_p=0.95, max_tokens=8):
+    completion = client_openai.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=temperature,
+        top_p=top_p,
+        max_tokens=max_tokens
+    )
+    response = completion.choices[0].message.content.strip()
+    return response
+
+
+def sample_validation_dataset(file_path, sample_size):
+    if file_path.endswith(".csv"):
+        df = pd.read_csv(file_path)
+    elif file_path.endswith(".parquet"):
+        df = pd.read_parquet(file_path)
+    elif file_path.endswith(".json"):
+        df = pd.read_json(file_path)
+    else:
+        raise ValueError("Invalid file format. Please provide a valid file format.")
+
+    sample_validation_df = df.sample(n=sample_size, random_state=3407)
+    sample_validation_df.reset_index(drop=True, inplace=True)
+    remain_df = df[~df["id"].isin(sample_validation_df["id"])].sample(frac=1, random_state=3407)
+    remain_df.reset_index(drop=True, inplace=True)
+
+    print(f"Sampled {sample_size} rows for validation dataset.")
+    print(f"Remaining {len(df) - sample_size} rows for training dataset.")
+
+    return sample_validation_df, remain_df
+
+
+def construct_prompt(df, msicl_num):
+    prompt = "You are tasked with evaluating and comparing answers from two large language models from a human's perspective to determine which one provides the better response. You will be given examples judged by humans, and you should learn from these examples to understand human preferences and finally make your own judgment. "
+    prompt += f"Here are {msicl_num} examples for you to learn from:\n"
+    for i in range(msicl_num):
+        if i != msicl_num - 1:
+            prompt += f"<Example {i+1}>\n"
+            prompt += f"<Question>\n{df['prompt'][i]}\n</Question>\n"
+            prompt += f"<Response A>\n{df['response_a'][i]}\n</Response A>\n"
+            prompt += f"<Response B>\n{df['response_b'][i]}\n</Response B>\n"
+            prompt += f"<Judgment>\n{df['winner'][i]}\n</Judgment>\n"
+            prompt += f"</Example {i+1}>\n\n"
+        else:
+            prompt += f"<Example {i+1}>\n"
+            prompt += f"<Question>\n{df['prompt'][i]}\n</Question>\n"
+            prompt += f"<Response A>\n{df['response_a'][i]}\n</Response A>\n"
+            prompt += f"<Response B>\n{df['response_b'][i]}\n</Response B>\n"
+            prompt += f"<Judgment>\n{df['winner'][i]}\n</Judgment>\n"
+            prompt += f"</Example {i+1}>\n"
+    prompt += "Now you need to evaluate the following question and responses based on the examples provided. Determine which response is better from a human perspective and return either 'model_a' or 'model_b' only. DO NOT include explanations.\n"
+    return prompt
